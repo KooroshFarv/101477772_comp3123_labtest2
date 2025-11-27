0b304a65381cf39f55843aff7991fd5e87244874
kooroshFarv <farvardin.koorosh@gmail.com>
2025-11-27 18:54:08 -0500

    added feels like + sunrise and fixed error and loading states

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ import WeatherCard from './components/WeatherCard';
 function App() {
   const [city, setCity] = useState('')
   const [weather, setWeather] = useState(null)
-
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
  
   const fetchWeather = async () => {
+    if (!city.trim()) return
     try {
+      setLoading(true)
+      setError(null)
       const res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
       );
 
       const data = await res.json();
-      if (data.cod === "404") {
-        alert("City not found");
+      if (data.cod === '404') {
+        setError('city not found !!')
+        setWeather(null)
         return;
       }
 
       setWeather(data);
     } catch (err) {
-      console.error(err);
+      setError('please try again')
+    } finally {
+      setLoading(false)
     }
   };
 
@@ -32,18 +39,20 @@ function App() {
           <input
             value={city}
             onChange={(e) => setCity(e.target.value)}
-            placeholder="Enter city..."
+            placeholder="Enter city"
             className="flex-1 bg-white/10 px-4 py-2 rounded-xl focus:outline-none placeholder-gray-300"
           />
           <button
             onClick={fetchWeather}
             className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded-xl"
           >
-            Search
+            {loading ? 'loading ..' : 'Search'}
           </button>
         </div>
 
-
+        {error && (
+          <div className='bg-red-700 rounded-lg mb-4 text-red-300 px-4 py-2 border-red-200/50'>{error}</div>
+        )}
 
         {weather && <WeatherCard weather={weather} />}
       </div>
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -5,18 +5,39 @@ const WeatherCard = ({weather}) => {
     const temp = Math.round(weather.main.temp)
     const desc = weather.weather[0]?.description
     const name = weather.name
-  return (
+    const feeling = Math.round(weather.main.feels_like)
+    const max = Math.round(weather.main.temp_max)
+    const sunset = weather.sys.sunset
+    const sunrise = weather.sys.sunrise
+
+    const timeFormat = (unix) => 
+        new Date(unix * 1000).toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit'
+        })
+    return (
     <div className='max-w-sm mx-auto mt-10 rounded-2xl bg-white/10 backdrop-blur-lg p-6 text-white shadow-xl'>
         <div className='flex items-center justify-between'>
             <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
             alt='icon'
-            className='w-20 h-20' />
+            className='w-20 h-20 bg-white/40 rounded-2xl'  />
         <h1 className="text-5xl font-bold">{temp}°C</h1>
         </div>
               <h3 className="text-2xl mt-2 font-semibold">{name}</h3>
               <p className="text-sm opacity-80 capitalize">{desc}</p>
 
        <div className="grid grid-cols-3 mt-6 text-center gap-3 text-sm">
+
+        <div className="bg-white/10 p-3 rounded-lg">
+            <p className="font-bold">{feeling}°C</p>
+            <p className="opacity-70 text-xs">Feels Like</p>
+        </div>
+
+          <div className="bg-white/10 p-3 rounded-lg">
+          <p className="font-bold">{max}°C</p>
+          <p className="opacity-70 text-xs">Max Temp</p>
+          </div>
+
         <div className="bg-white/10 p-3 rounded-lg">
           <p className="font-bold">{weather.wind.speed} km/h</p>
           <p className="opacity-70 text-xs">Wind</p>
@@ -28,6 +49,15 @@ const WeatherCard = ({weather}) => {
         <div className="bg-white/10 p-3 rounded-lg">
           <p className="font-bold">{weather.clouds.all}%</p>
           <p className="opacity-70 text-xs">Clouds</p>
+
+        </div>
+             <div className="bg-white/10 p-3 rounded-lg">
+          <p className="font-bold">{timeFormat(sunrise)}</p>
+          <p className="opacity-70 text-xs">Sunrise</p>
+        </div>
+           <div className="bg-white/10 p-3 rounded-lg">
+          <p className="font-bold">{timeFormat(sunset)}</p>
+          <p className="opacity-70 text-xs">Sunset</p>
         </div>
       </div>
     </div>
